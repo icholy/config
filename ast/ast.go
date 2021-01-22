87930d8225ccf079fac495f16e7dd9e34b50234f
Ilia Choly <ilia.choly@gmail.com>
2021-01-22 13:51:34 -0500

    start the ast

---
+++ ast/ast.go
@@ -0,0 +1,47 @@
+package ast
+
+import "go/token"
+
+// Value
+type Value interface{ value() }
+
+// Block is a collection of entries
+type Block struct {
+	Start   token.Pos
+	Entries []*Entry
+}
+
+func (Block) value() {}
+
+type Ident struct {
+	Start token.Pos
+	Value string
+}
+
+type Number struct {
+	Start token.Pos
+	Value float64
+}
+
+func (Number) value() {}
+
+type Bool struct {
+	Start token.Pos
+	Value bool
+}
+
+func (Bool) value() {}
+
+type String struct {
+	Start token.Pos
+	Value string
+}
+
+func (String) value() {}
+
+// Entry is a key/value pair
+type Entry struct {
+	Start token.Pos
+	Name  Ident
+	Value Value
+}
