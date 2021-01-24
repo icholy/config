dbe97b6b31f8891c897c7e7b879aa7294e58c776
Ilia Choly <ilia.choly@gmail.com>
2021-01-24 14:18:20 -0500

    add failing test for parsing string entry

--- ast/parse_test.go
+++ ast/parse_test.go
@@ -27,6 +27,18 @@ func TestParse(t *testing.T) {
 				},
 			},
 		},
+		{
+			name:  "StringEntry",
+			input: `bar = "test"`,
+			expect: &Block{
+				Entries: []*Entry{
+					{
+						Name:  &Ident{Value: "bar"},
+						Value: &String{Value: "test"},
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
