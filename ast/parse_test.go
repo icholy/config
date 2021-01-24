beebe63d646197e5914849c490feacd738d595b9
Ilia Choly <ilia.choly@gmail.com>
2021-01-24 15:07:59 -0500

    add failing test for empty block

--- ast/parse_test.go
+++ ast/parse_test.go
@@ -109,6 +109,18 @@ func TestParse(t *testing.T) {
 				},
 			},
 		},
+		{
+			name:  "EmptyBlock",
+			input: `block {}`,
+			expect: &Block{
+				Entries: []*Entry{
+					{
+						Name:  &Ident{Value: "block"},
+						Value: &Block{},
+					},
+				},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
