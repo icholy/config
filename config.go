5f7cbb530f7ddcdf27ac0964b3c8b356e5ae1ad8
Ilia Choly <ilia.choly@gmail.com>
2021-02-16 11:38:58 -0500

    first passing test

---
+++ config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/icholy/config/ast"
+)
+
+// Unmarshal ...
+func Unmarshal(data []byte, v interface{}) error {
+	block, err := ast.Parse(string(data))
+	if err != nil {
+		return err
+	}
+	return decodeValue(block, reflect.ValueOf(v))
+}
+
+func decodeBlock(b *ast.Block, dst reflect.Value) error {
+	return decodeBlockToMap(b, dst)
+}
+
+func decodeBlockToMap(b *ast.Block, dst reflect.Value) error {
+	for _, e := range b.Entries {
+		elem := reflect.New(dst.Type().Elem()).Elem()
+		if err := decodeValue(e.Value, elem); err != nil {
+			return err
+		}
+		dst.SetMapIndex(reflect.ValueOf(e.Name.Value), elem)
+	}
+	return nil
+}
+
+func decodeNumber(n *ast.Number, dst reflect.Value) error {
+	dst.Set(reflect.ValueOf(n.Value))
+	return nil
+}
+
+func decodeValue(v ast.Value, dst reflect.Value) error {
+	for dst.Kind() == reflect.Ptr {
+		dst = reflect.Indirect(dst)
+	}
+	switch v := v.(type) {
+	case *ast.Block:
+		return decodeBlock(v, dst)
+	case *ast.Number:
+		return decodeNumber(v, dst)
+	default:
+		return fmt.Errorf("not implemented: %T", v)
+	}
+}
---
+++ config_test.go
@@ -0,0 +1,39 @@
+package config
+
+import (
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestUnmarshal(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		dst   func() interface{}
+		want  func() interface{}
+	}{
+		{
+			input: "a=123\nb=42",
+			dst: func() interface{} {
+				m := map[string]interface{}{}
+				return &m
+			},
+			want: func() interface{} {
+				m := map[string]interface{}{
+					"a": float64(123),
+					"b": float64(42),
+				}
+				return &m
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, want := tt.dst(), tt.want()
+			err := Unmarshal([]byte(tt.input), got)
+			assert.NilError(t, err)
+			assert.DeepEqual(t, got, want)
+		})
+	}
+}
